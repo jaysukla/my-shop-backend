41b48cdb60ae4afd65fe8b42c24db527c9efc9a4
jaysukla <am7343249@gmail.com>
2023-04-21 00:18:35 +0530

    cors

--- index.js
+++ index.js
@@ -6,9 +6,9 @@ const bcrypt = require('bcrypt');
 
 const {watchman} = require('./middlewere/watchman')
 const app = express()
-app.use(cors())
-app.use(express.json())
 
+app.use(express.json())
+app.use(cors())
 
 
 
